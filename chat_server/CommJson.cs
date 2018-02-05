b0c60938aacf73951db24ae180cae583208a02dc
Lars Wallberg <Wallberg.Lars.net@gmail.com>
2018-02-05 20:15:15 +0100

    Projektdateien hinzufügen.

---
+++ chat_server/CommJson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace chat_server
+{
+    public class CommJson
+    {
+        public string FromC { get; set; }
+        public string ToC { get; set; }
+        public string MSG { get; set; }
+    }
+}
