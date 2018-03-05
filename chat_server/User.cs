a99b0385a86c41ba32213b8682fde5460c4f7d52
Lars Wallberg <Wallberg.Lars.net@gmail.com>
2018-03-05 19:16:49 +0100

    Initial Commit

---
+++ chat_server/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace chat_server
+{
+    [Table("Users")]
+    class User
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        [Required]
+        public IPAddress IpAdress { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public DateTime GeneratedAt { get; set; }
+        public string Password { get; set; }
+
+    }
+}
